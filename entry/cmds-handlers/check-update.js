54d4720bc99d4c9858598e8c175861d42173a8b7
MHalo <chinatmall@outlook.com>
2024-08-22 20:03:29 +0800

    Update check-update.js

--- entry/cmds-handlers/check-update.js
+++ entry/cmds-handlers/check-update.js
@@ -8,7 +8,10 @@ export default {
             .description('check update')
             .option('-d, --detail [value]', 'update notifier detail', false)
             .action(async (options) => {
-                const notifier = updateNotifier({ pkg });
+                const notifier = updateNotifier({ 
+                    pkg,
+                    updateCheckInterval: 1000 * 60 *60 /**每小时检查一次 */
+                 });
                 if(notifier.update){
                     notifier.notify()
                 }
